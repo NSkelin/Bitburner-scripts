5b0e995b659b214e4aed6489386aaa2d962fba82
Nick <nick.skelin@gmail.com>
2023-10-08 21:23:43 -0700

    add primeServersForHack to startup scripts

--- src/startup.ts
+++ src/startup.ts
@@ -3,5 +3,6 @@ import {NS} from "@ns";
 export async function main(ns: NS) {
   if (!ns.scriptRunning("improveHacknet.js", "home")) ns.run("improveHacknet.js");
   if (!ns.scriptRunning("nukem.js", "home")) ns.run("nukem.js");
+  if (!ns.scriptRunning("primeServersForHack.js", "home")) ns.run("primeServersForHack.js");
   //   if (!ns.scriptRunning("unlockFactions.js", "home")) ns.run("unlockFactions.js");
 }
