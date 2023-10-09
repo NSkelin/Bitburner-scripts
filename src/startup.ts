f35e251e81add4819051ffe39487f9edf89a0e73
Nick <nick.skelin@gmail.com>
2023-10-09 16:43:02 -0700

    add buy servers to startup

--- src/startup.ts
+++ src/startup.ts
@@ -2,7 +2,7 @@ import {NS} from "@ns";
 
 export async function main(ns: NS) {
   if (!ns.scriptRunning("improveHacknet.js", "home")) ns.run("improveHacknet.js");
+  if (!ns.scriptRunning("buyServers.js", "home")) ns.run("buyServers.js");
   if (!ns.scriptRunning("nukem.js", "home")) ns.run("nukem.js");
   if (!ns.scriptRunning("primeServersForHack.js", "home")) ns.run("primeServersForHack.js");
-  //   if (!ns.scriptRunning("unlockFactions.js", "home")) ns.run("unlockFactions.js");
 }
