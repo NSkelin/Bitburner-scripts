4a692fbf2e99954cf82dd0e18f5b806f8b3991e1
Nick <nick.skelin@gmail.com>
2023-10-10 22:37:25 -0700

    add sort selection add sort by some columns by adding a arg

--- src/infiltrator.ts
+++ src/infiltrator.ts
@@ -1,6 +1,29 @@
 import {NS} from "@ns";
 import {printTable} from "./lib/logger";
 
+/** Sorts the infiltration data by a specific column in descending order.
+ *
+ * @param data The infiltration data
+ * @param sortBy The column to sort by
+ */
+function sortData(ns: NS, data: string[][], sortBy: string) {
+  const sortOptions = {
+    difficulty: 0,
+    traderep: 3,
+    soarep: 4,
+  };
+
+  let option: number;
+  if (sortBy === "difficulty" || sortBy === "traderep" || sortBy === "soarep") {
+    option = sortOptions[sortBy];
+  } else {
+    ns.tprint(`${sortBy} is not a valid option, canceling sort.\nValid options:\n${Object.keys(sortOptions)}`);
+  }
+
+  // sort in descending order.
+  data.sort((a, b) => Number(b[option]) - Number(a[option]));
+}
+
 export async function main(ns: NS) {
   const locals = ns.infiltration.getPossibleLocations();
   const arr = [["difficulty", "city", "company", "tradeRep gain", "SoARep gain"]];
@@ -16,9 +39,9 @@ export async function main(ns: NS) {
     arr.push([diff, city, company, tradeRep, SoARep]);
   }
 
-  // arr.sort((a, b) => b.diff - a.diff);
-  // arr.sort((a, b) => b.tradeRep - a.tradeRep);
-  arr.sort((a, b) => Number(b[4]) - Number(a[4]));
+  let sortBy = ns.args[0];
+  if (typeof sortBy !== "string") sortBy = "soarep";
+  sortData(ns, arr, sortBy);
 
   printTable(ns, arr, true);
 }
