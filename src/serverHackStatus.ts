a20c899b5420379556686376fc6f1deaef26af3d
Nick <nick.skelin@gmail.com>
2023-10-14 18:26:58 -0700

    add server status

--- src/serverHackStatus.ts
+++ src/serverHackStatus.ts
@@ -2,10 +2,37 @@ import {NS} from "@ns";
 import {forEachServer} from "./lib/helpers";
 import {createProgressBar, tprintTable} from "./lib/logger";
 
+async function getServerStatus(ns: NS) {
+  const status = new Map<string, string>();
+  await forEachServer(
+    ns,
+    (ns, serverName) => {
+      for (const script of ns.ps(serverName)) {
+        const filename = script.filename;
+        const target = script.args[0];
+
+        if (typeof target !== "string") continue;
+
+        if (filename === "minHacker.js") {
+          status.set(target, "Hacking");
+        } else if (filename === "minGrower.js" && status.get(target) !== "Hacking") {
+          status.set(target, "Growing");
+        } else if (filename === "minWeakener.js" && status.get(target) !== "Hacking" && status.get(target) !== "Growing") {
+          status.set(target, "Weakening");
+        }
+      }
+    },
+    {includeHomeServer: true, includePurchasedServers: true}
+  );
+
+  return status;
+}
+
 /** Prints a table summary of all unowned servers to the terminal. */
 export async function main(ns: NS) {
   const data: string[][] = [];
-  const tableHeaders = ["Server Name", "Money", "Max Money", "Security", "Root access", "Ports", "RAM", "Hack skill"];
+  const tableHeaders = ["Server Name", "status", "Money", "Max Money", "Security", "Root access", "Ports", "RAM", "Hack skill"];
+  const serverStatus = await getServerStatus(ns);
 
   const createRow = (ns: NS, serverName: string) => {
     // server stats.
@@ -21,6 +48,7 @@ export async function main(ns: NS) {
     // assemble the table row for this server.
     const row = [
       serverName,
+      serverStatus.get(serverName) ?? "NA",
       `${moneyBar}`,
       `${ns.formatNumber(ns.getServerMaxMoney(serverName))}`,
       `${securityBar}`,
