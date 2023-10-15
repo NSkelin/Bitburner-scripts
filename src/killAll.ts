92846a70fba519b0b3ac684189acf69bfd67ace4
Nick <nick.skelin@gmail.com>
2023-10-15 13:23:27 -0700

    change script to a blacklist instead of whitelist Rather than add each new script i write, only add that ones i want to keep running

--- src/killAll.ts
+++ src/killAll.ts
@@ -3,12 +3,12 @@ import {forEachServer} from "./lib/helpers";
 
 /** Copies and executes the hack file on all servers with root access at depth from the current server */
 export async function main(ns: NS) {
-  const scripts = ["minHacker.js", "minGrower.js", "minWeakener.js", "hackServer.js", "primeServersForHack.js"];
-
-  // ns.killall()
+  const blacklist = ["buyServers.js", "improveHacknet.js"];
+  const scripts = ns.ls("home", ".js");
 
   const callback = (ns: NS, serverName: string) => {
     for (const script of scripts) {
+      if (blacklist.includes(script)) continue;
       ns.scriptKill(script, serverName);
     }
   };
