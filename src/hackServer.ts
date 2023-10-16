e6ec720e8619201febb0eb04047d79445d423932
Nick <nick.skelin@gmail.com>
2023-10-16 03:02:23 -0700

    refactor and update documentation

--- src/hackServer.ts
+++ src/hackServer.ts
@@ -14,89 +14,96 @@ const allocationOptions: AllocateScriptsOptions = {
   },
 };
 
-/** Lowers a servers security to its minimum level possible.
+/** Waits for a given condition to return true before then stops all the given scripts. */
+async function stopScriptsAfterCondition(ns: NS, condition: () => boolean, scripts: {pid: number; server: string}[]) {
+  while (condition() === false) {
+    await ns.sleep(5000);
+  }
+
+  for (const {pid} of scripts) {
+    ns.kill(pid);
+  }
+}
+
+/** Lowers the target servers security to its minimum possible level.
  *
- * Runs a script on the server to lower its security to the minimum value possible, then stops the script and returns.
+ * Calculates the threads required to minimize the servers security to its minimum, then runs as many threads as possible.
+ * Once the servers security is at its minimum the weakener script(s) are automatically stopped.
+ * @param server The server to lower security on.
  */
 async function weakenSecurityToMinimum(ns: NS, server: string) {
-  // get security to minimum
+  // server security already min, exit early.
   if (serverSecurityAtMinimum(ns, server) === true) return;
 
+  // get thread count needed to minimize security level.
   const securityLevelLeft = ns.getServerSecurityLevel(server) - ns.getServerMinSecurityLevel(server);
   let threadCount = 1;
   while (securityLevelLeft > ns.weakenAnalyze(threadCount)) {
     threadCount++;
   }
 
+  // Try to start weakener scripts.
   const scriptPids = await allocateScripts(ns, [{script: "minWeakener.js", threads: threadCount, args: [server]}], allocationOptions);
+  if (scriptPids == null || scriptPids.length === 0) return;
 
-  if (scriptPids == null || scriptPids.length === 0) {
-    return;
-  }
-
-  // wait till done
-  while (serverSecurityAtMinimum(ns, server) === false) {
-    await ns.sleep(5000);
-  }
-
-  for (const {pid} of scriptPids) {
-    ns.kill(pid);
-  }
-
-  return;
+  // wait until the servers security is at its minimum.
+  await stopScriptsAfterCondition(ns, () => serverSecurityAtMinimum(ns, server), scriptPids);
 }
 
+/** Grows the target servers money to its maximum possible amount while keeping the security to a minimum.
+ *
+ * Calculates the threads required to grow the servers money by 10%, then runs as many threads as possible.
+ * Once the servers money is at its maximum the grower script(s) are automatically stopped.
+ * @param server The server to grow money on.
+ */
 async function growMoneyToMaximum(ns: NS, server: string) {
-  // do calc
-  const {growthThreads, weakenThreads} = getOptimalHackingThreads(ns, server, 0.1);
+  // server money already max, exit early.
+  if (serverMoneyAtMax(ns, server) === true) return;
 
-  // grow to max & keep security at minimum
+  // get thread count needed to grow money by 10%.
+  const {growthThreads, weakenThreads} = getOptimalHackingThreads(ns, server, 0.1);
+  if (growthThreads === 0 || weakenThreads === 0) return;
+
+  // Try to start grower scripts.
   const scripts = [
     {script: "minGrower.js", threads: growthThreads, args: [server]},
     {script: "minWeakener.js", threads: weakenThreads, args: [server]},
   ];
-
   const scriptPids = await allocateScripts(ns, scripts, allocationOptions);
+  if (scriptPids == null || scriptPids.length === 0) return;
 
-  if (scriptPids == null || scriptPids.length === 0) {
-    return;
-  }
-
-  // wait till done
-  while (serverMoneyAtMax(ns, server) === false) {
-    await ns.sleep(5000);
-  }
-
-  for (const {pid} of scriptPids) {
-    ns.kill(pid);
-  }
-
-  return;
+  // wait until the servers money is at its maximum.
+  await stopScriptsAfterCondition(ns, () => serverMoneyAtMax(ns, server), scriptPids);
 }
 
+/** Continously hacks the target server for 10% of its money while maintaining its money at maximum and security at minimum.
+ * @param target The server to be hacked.
+ */
 async function hackTarget(ns: NS, target: string) {
+  // get thread count needed to maintain hack.
   const {hackThreads, growthThreads, weakenThreads} = getOptimalHackingThreads(ns, target, 0.1);
   if (hackThreads === 0 || growthThreads === 0 || weakenThreads === 0) return;
+
+  // Try to start hacking scripts.
   const scripts = [
     {script: "minHacker.js", threads: hackThreads, args: [target]},
     {script: "minGrower.js", threads: growthThreads, args: [target]},
     {script: "minWeakener.js", threads: weakenThreads, args: [target]},
   ];
-
   await allocateScripts(ns, scripts, allocationOptions);
-  return;
 }
 
-/** This function will prime a server for hacking by lowering its security to the minimum and
- * growing its available money to the maximum. */
+/** Primes a server for hacking by lowering its security to the minimum and
+ * growing its available money to the maximum.
+ * @param server The server to prime for hacking.
+ */
 async function primeServer(ns: NS, server: string) {
   await weakenSecurityToMinimum(ns, server);
   await growMoneyToMaximum(ns, server);
   await weakenSecurityToMinimum(ns, server); // redo to ensure security is minimum.
-  await hackTarget(ns, server);
 }
 
-/** An incredibly cheap server security lower meant to be controlled by another script. */
+/** Prepares a server for hacking, then hacks it. */
 export async function main(ns: NS) {
   const server = ns.args[0];
 
@@ -106,4 +113,5 @@ export async function main(ns: NS) {
   }
 
   await primeServer(ns, server);
+  await hackTarget(ns, server);
 }
