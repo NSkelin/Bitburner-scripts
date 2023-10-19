a7c1484ed307dc9979e16b1c56b4f4a50108df0f
Nick <nick.skelin@gmail.com>
2023-10-19 00:38:45 -0700

    update server name

--- src/startup.ts
+++ src/startup.ts
@@ -4,5 +4,5 @@ export async function main(ns: NS) {
   if (!ns.scriptRunning("improveHacknet.js", "home")) ns.run("improveHacknet.js");
   if (!ns.scriptRunning("buyServers.js", "home")) ns.run("buyServers.js");
   if (!ns.scriptRunning("nukem.js", "home")) ns.run("nukem.js");
-  if (!ns.scriptRunning("primeServersForHack.js", "home")) ns.run("primeServersForHack.js");
+  if (!ns.scriptRunning("hackAllServers.js", "home")) ns.run("hackAllServers.js");
 }
