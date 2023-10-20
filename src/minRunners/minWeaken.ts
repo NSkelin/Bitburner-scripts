9eed8df1c3c7f1931ea31cd7578f2643808f0147
Nick <nick.skelin@gmail.com>
2023-10-20 02:49:38 -0700

    Add optional one use mode

--- src/minRunners/minWeaken.ts
+++ src/minRunners/minWeaken.ts
@@ -3,13 +3,18 @@ import {NS} from "@ns";
 /** An incredibly cheap server security lower meant to be controlled by another script. */
 export async function main(ns: NS) {
   const server = ns.args[0];
+  const loop = ns.args[1] ?? true;
 
   if (typeof server !== "string") {
     ns.tprint("arg[0] is not of type string!");
+    return;
+  } else if (typeof loop !== "boolean") {
+    ns.tprint("arg[1] is not of type string!");
     return;
   }
 
   while (true) {
     await ns.weaken(server);
+    if (!loop) break;
   }
 }
