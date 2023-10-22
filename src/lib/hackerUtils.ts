d59067df4221084c060fd680ea5228e25e1e84eb
Nick <nick.skelin@gmail.com>
2023-10-21 17:18:46 -0700

    move function

--- src/lib/hackerUtils.ts
+++ src/lib/hackerUtils.ts
@@ -1,4 +1,45 @@
 import {NS} from "@ns";
+import {forEachServer} from "lib/helpers";
+
+/** Returns a summary of the scripts that best represents each targets current hack stage.
+ *
+ * A targets hack stage goes in this order: 1. Hack, 2: Grow, 3: Weaken, 4: Deploying, 5: Null. For each target only the
+ * scripts related to the highest order will be kept.
+ * Example: Target: n00dles. Scripts targeting n00dles: hack.js, weaken.js, deploy.js. Stage: "hack". Saved scripts: hack.js.
+ *
+ * It will search through the "home" and purchased servers for any running scripts that are related to hacking.
+ * Each target will have the most relevant (see above) script and the total thread count for all instances of that script with the same target.
+ */
+export async function getHackStage(ns: NS) {
+  const stageMap = new Map<string, {script: string; threads: number}>();
+  // ORDER MATTERS. The lower the index, the higher the stage.
+  const hackingScripts = ["minRunners/minHack.js", "minRunners/minGrow.js", "minRunners/minWeaken.js", "hackServer.js"];
+
+  const callBack = (ns: NS, serverName: string) => {
+    for (const {filename, args, threads} of ns.ps(serverName)) {
+      // Incorrect script.
+      if (!hackingScripts.includes(filename)) continue;
+      const target = args[0];
+      // Args[0] cant be a target server name.
+      if (typeof target !== "string") continue;
+
+      const {threads: threadCount, script} = stageMap.get(target) ?? {threads: 0, script: null};
+
+      // Create a new stage for targets not in the map yet / higher order stage found.
+      if (script == null || hackingScripts.indexOf(filename) < hackingScripts.indexOf(script)) {
+        stageMap.set(target, {script: filename, threads: threads});
+      } else if (filename === script) {
+        // Update thread count.
+        stageMap.set(target, {script: filename, threads: threads + threadCount});
+      }
+    }
+  };
+
+  // Get each servers hack status.
+  await forEachServer(ns, callBack, {includeHomeServer: true, includePurchasedServers: true});
+
+  return stageMap;
+}
 
 /** Returns true if the server's available money is at its maximum amount. */
 export function serverMoneyAtMax(ns: NS, server: string) {
